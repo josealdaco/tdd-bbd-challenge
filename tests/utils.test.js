9580bced65483379709acb7549138ea8462084bf
siko408 <gamerbloxhd@gmail.com>
2020-04-15 20:39:34 -0700

    challenges completed

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -20,11 +20,20 @@ it("should say hello", function() {
 // Level 1 Challenges
 // ========================================================
 
-it("should return the area of a 5 by 6 rectangle")
+it("should return the area of a 5 by 6 rectangle", function(){
+    const area = utils.area(5,6)
+    expect(area).to.equal(30)
+})
 
-it("should return the perimeter of a 5 by 6 rectangle")
+it("should return the perimeter of a 5 by 6 rectangle", function(){
+    const perimeter = utils.perimeter(5,6)
+    expect(perimeter).to.equal(22)
+})
 
-it("should return the are of a circle of radius 5")
+it("should return the are of a circle of radius 5", function(){
+    const circleArea = utils.circleArea(5)
+    expect(circleArea).to.equal(78.53981633974483)
+})
 
 // ========================================================
 // Level 2 Challenges
@@ -33,16 +42,41 @@ it("should return the are of a circle of radius 5")
 // of "Pending Tests" in Chai. Someone should write these
 // tests eventually.
 // ========================================================
+it("Checking the area", function(){
+    const area  = utils.circleArea(5)
+    expect(area).to.equal(78.53981633974483)
+})
+it("Should create a new (object) Item with name and price", function(){
+    const item = utils.createItem("apple",0.99)
+    expect(item).to.be.a("object")
+    expect(item).to.have.property("name", "apple")
+    expect(item).to.have.property("price", 0.99)
+})
 
-it("Should create a new (object) Item with name and price")
+it("Should return an array containing all items in cart", function(){
+    const shoppingcartarray = utils.createShoppingcart()
+    expect(shoppingcartarray).to.be.a("array")
+})
 
-it("Should return an array containing all items in cart")
+it("Should add a new item to the shopping cart", function(){
+    const additem = utils.additem("apple", 1.00)
+    const cartsize = utils.createShoppingcart()
+    expect(cartsize.length).to.equal(1)
+})
 
-it("Should add a new item to the shopping cart")
+it("Should return the number of items in the cart", function(){
+    const count = utils.cartSize()
+    expect(count).to.equal(1)
+})
 
-it("Should return the number of items in the cart")
+it("Should remove items from cart", function(){
+    const product = "apple"
+    const count = utils.cartSize()
+    utils.removeItem(product)
+    expect(count).to.equal(1)
+    expect(utils.cartSize()).to.equal(0)
+})
 
-it("Should remove items from cart")
 
 // ========================================================
 // Stretch Challenges
--- utils.js
+++ utils.js
@@ -1,7 +1,6 @@
 // ========================================================
 // Level 1 Challenges
 // ========================================================
-
 const sayHello = () => {
   return "Hello"
 }
@@ -24,10 +23,36 @@ const circleArea = r => {
 // NOTE: You will need to implement methods below (not yet
 // defined) in order to make the tests pass.
 // ========================================================
+const shoppingcart = []
+const createItem = (name, price)=>{
+    const object = {"name": name, "price": price}
+    return object
+}
+const createShoppingcart = ()=>{
+    return shoppingcart
+}
+const additem = (name, price) =>{
+    shoppingcart.push(createItem(name,price))
+}
+const cartSize = () =>{
+    return shoppingcart.length
+}
+const removeItem = (name) =>{
+    index = undefined
+    for (i=0;i<shoppingcart.length; i++){
+        if (shoppingcart[i].name == name){
+            index = i
+        }
+    }
+    if (index != undefined && index > -1) {
+     shoppingcart.splice(index, 1);
+ }else {
+     console.log("PRODUCT NOT FOUND", index)
+ }
+    return
+}
 
 
-
-
-module.exports = { 
-  sayHello, area, perimeter, circleArea
+module.exports = {
+  sayHello, area, perimeter, circleArea,createItem,createShoppingcart, additem, cartSize, removeItem
 }
